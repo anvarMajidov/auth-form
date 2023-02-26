e028a4216edd7490348b9dd1f455fa18c5a5eefc
anvar <majidovanvar30.11.2002@gmail.com>
2023-02-26 21:17:20 +0500

    added helper class for validation input fields, and registered DI container

---
+++ WinFormsApp/Helpers/FieldValidator.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WinFormsApp.Helpers;
+
+public static class FieldValidator
+{
+    public static bool IsUsernameValid(string username)
+    {
+        if (string.IsNullOrEmpty(username))
+        {
+            MessageBox.Show("Please enter a username.");
+            return false;
+        }
+
+        var regex = new Regex(@"^[a-zA-Zа-яА-Я]+$");
+        if (!regex.IsMatch(username))
+        {
+            MessageBox.Show("Username must contain only cyrillic or latin letters");
+            return false;
+        }
+
+        if (username.Length < 8)
+        {
+            MessageBox.Show("Username must be at least 8 characters long.");
+            return false;
+        }
+        return true;
+    }
+
+    public static bool IsPasswordValid(string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            MessageBox.Show("Please enter a password.");
+            return false;
+        }
+
+        if (password.Length < 8)
+        {
+            MessageBox.Show("Password must be at least 8 characters long.");
+            return false;
+        }
+        return true;
+    }
+
+    public static bool DoPasswordsMatch(string p1, string p2)
+    {
+        if (p1.Equals(p2)) return true;
+        
+        MessageBox.Show("Passwords do not match");
+        
+        return false;
+    }
+}
--- WinFormsApp/Program.cs
+++ WinFormsApp/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+using WinFormsApp.Data;
 using WinFormsApp.Forms;
+using WinFormsApp.Interfaces.Repositories;
+using WinFormsApp.Interfaces.Services;
+using WinFormsApp.Repositories;
+using WinFormsApp.Services;
 
 namespace WinFormsApp;
 
@@ -10,7 +16,32 @@ static class Program
     [STAThread]
     static void Main()
     {
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Login());
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        using var client = new AppDbContext();
+        client.Database.EnsureCreated();
+
+        using var serviceProvider = services.BuildServiceProvider();
+        var mainForm = serviceProvider.GetRequiredService<Login>();
+        
+        Application.Run(mainForm);
+    }
+    
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEntityFrameworkSqlite()
+            .AddDbContext<AppDbContext>();
+
+        services.AddScoped<Login>();
+        services.AddScoped<Register>();
+        services.AddScoped<Home>();
+
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
+        services.AddScoped<IUserRepository, UserRepository>();
     }
 }
